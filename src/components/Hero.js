40637b38fdb62b2607d91fef6bf5333697ec8bb8
yaj-23 <yajurva.trivedi@ryerson.ca>
2020-12-24 19:09:40 -0500

    Glitch Text Addition

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import'./Hero.css';
+
+ 
+function Hero(){
+  return(
+    <div className ='hero-container'>
+      <h1 className="glitch">
+        <span aria-hidden ="true">Yajurva</span>
+        <span>Yajurva</span>
+        <span aria-hidden="true">Yajurva</span>
+      
+      
+      </h1>
+    </div> 
+  )
+}
+
+export default Hero;
