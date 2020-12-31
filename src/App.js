6c1ef0225e5f8c988e63c7fd0a51b1b402ae0042
yaj-23 <yajurva.trivedi@ryerson.ca>
2020-12-30 23:37:41 -0500

    idek2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './components/pages/Home';
+import Home from './components/pages/HomePage/Home';
 
 function App(){
     return(
         <>
             <Router>
-                
+                <Navbar/>
                 <Switch>
-                    <Route path ='/' exact component={Home}/>
+                    <Route path ='/' exact component={Home} />
 
                 </Switch>
 
