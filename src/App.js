e92334820a902400be029b6435cc672e0e1da271
yaj-23 <yajurva.trivedi@ryerson.ca>
2021-01-04 03:24:00 -0500

    Navbar-AlmostDone, StartedHero

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Hero from './components/pages/Hero';
 
 function App(){
     return(
@@ -9,7 +10,7 @@ function App(){
             <Router>
                 <Navbar/>
                 <Switch>
-                    <Route path ='/' />
+                    <Route path ='/' exact component={Hero} />
 
                 </Switch>
 
--- src/components/pages/Hero.js
+++ src/components/pages/Hero.js
@@ -4,11 +4,9 @@ import './Hero.css';
 function Hero() {
     return (
         <>
-            <div className='hero'>
-                <div>
-                    
-                </div>
-
+            <div className='hero-container'>
+                <h1 className="greetings">Greetings</h1>
+                <p className="name">I'm Yajurva Trivedi</p>
             </div>            
         </>
     )
