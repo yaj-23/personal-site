6e798c8480cb7fdca2e46ceaa15a1c93d098b881
yaj-23 <yajurva.trivedi@ryerson.ca>
2021-01-05 00:25:48 -0500

    added Button Componenet

--- src/components/pages/Hero.js
+++ src/components/pages/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css';
+import {Button} from '../Button.js';
 
 function Hero() {
     return (
@@ -7,6 +8,27 @@ function Hero() {
             <div className='hero-container'>
                 <h1 className="greetings">Greetings</h1>
                 <p className="name">I'm Yajurva Trivedi</p>
+                
+                <div className='buttons'>
+                    <div className='button-one'>                
+                        <Button buttonStyle='btn-primary' buttonColor='primary' buttonSize='btn-medium'>
+                            About Me
+                        </Button>
+                    </div>
+
+                    <div className='button-two'>                
+                        <Button buttonStyle='btn-primary' buttonColor='primary' buttonSize='btn-medium'>
+                            Projects
+                        </Button>
+                    </div>
+
+                    <div className='button-three'>                
+                        <Button buttonStyle='btn-primary' buttonColor='primary' buttonSize='btn-medium'>
+                            Contact Me
+                        </Button>
+                    </div>
+                </div>
+                
             </div>            
         </>
     )
