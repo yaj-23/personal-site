ee173b68a7cf86886b7c7fec267426bc836fc14f
yaj-23 <yajurva.trivedi@ryerson.ca>
2021-01-06 04:17:19 -0500

    about_section_Started_done_web_version

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, {useEffect, useRef}from 'react';
+import {Link} from 'react-router-dom';
+
 import './About.css';
+import lottie from 'lottie-web';
+import {Button} from '../Button.js';
+
+
 
 
 
 function About() {
+
+    const animone = useRef(null)
+
+    useEffect(() =>{
+        lottie.loadAnimation({
+            container : animone.current,
+            renderer:'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../about.json')
+        })
+    }, [] )
     return (
         <>
             <div className="about-container">
                 <h1 className='header'>
                     About Me
                 </h1>
+                <div className='cont'>
+                    <p1 className="para-first">I am an upcoming software enthusiast based in Toronto, Canada. 
+                        I am currently in my second year in Computer Engineering at
+                        Ryerson University located in the heart of downtown Toronto.I'm a huge basketball fanatic,
+                        I enjoy competing in fantasy leagues with friends. Few things about me everyone should know
+                        I'm a sucker for modern and sleek design, and I love learning new ways to test and apply knowledge.
+                    </p1>
 
-                <p1>I AM A SECOND YEAR
-                     COMPUTER ENGINEERING STUDENT STUDYING AT RYERSON UNIVERSITY, 
-                     SPECIALIZING IN COMPUTER ENGINEERING.</p1>
+                    
+                    <div className="animone" ref={animone}></div>
+                </div>  
+
+                <div className='buttons'>
+                    <Link to='/'  className='button-about'>           
+                        <Button buttonStyle='btn-primary' buttonColor='about' buttonSize='btn-medium'>
+                            Contact Me
+                        </Button>
+                    </Link>
+                </div>
+                
+            
             </div>
+
+
+
         </>
     )
 }
