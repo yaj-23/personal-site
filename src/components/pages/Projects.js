89bc521d85093aae14c0397d56ede24598adfb3b
yaj-23 <yajurva.trivedi@ryerson.ca>
2021-01-06 23:51:37 -0500

    projectsupdate

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -9,6 +9,27 @@ function Projects() {
                     Projects
                 </h1>
 
+                <div className="sec-container">
+                    <div className="section-one">
+                        <img></img>
+                        <h1>Personal React Website</h1>
+                        <p1>Using React JS, HTML and CSS, created this completely responsive website.</p1>
+                    </div>
+
+                    <div className="section-two">
+                        <img></img>
+                        <h1>Ryerson Engineering Competition Website</h1>
+                        <p1>Using React Js, HTML 5, CSS, created a project showcase website building on REC Competition</p1>
+                    </div>
+                    <div className="section-three">
+                        <img></img>
+                        <h1>Xplor TO</h1>
+                        <p1></p1>
+
+                    </div>
+
+                </div>
+
             </div>
         </>      
     )
